3a77fcdc5e91cc4e1808270297512059ed6dddfd
anaquinterog <ana.quinterog@udem.edu>
2024-04-04 19:49:59 -0600

    New images

--- monocular-camera-calibration.py
+++ monocular-camera-calibration.py
@@ -10,7 +10,7 @@
     Last updated: Saturday 23 March 2024
 
     EXAMPLE OF USAGE:
-    python monocular-camera-calibration.py --path_to_calibration_images sample-calibration-images/ \
+    python3 monocular-camera-calibration.py --path_to_calibration_images sample-calibration-images/ \
     --calibration_image_format JPG \
     --chessboard_size 9 6 \
     --output_calibration_parameters calibration-parameters/calibration_data.json
